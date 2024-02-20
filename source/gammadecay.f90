b45cb7211dd9ae0f705a86d8393e77dca4f31b60
Arjan Koning <arjankoning1@gmail.com>
2024-02-20 18:46:37 +0100

    More consistent YANDF format for discrete gamma decay

--- source/gammadecay.f90
+++ source/gammadecay.f90
@@ -43,29 +43,23 @@ subroutine gammadecay(Zix, Nix)
 ! *** Declaration of local data
 !
   implicit none
-  character(len=3)  :: massstring !
   character(len=6)  :: resstring !
   character(len=6)  :: finalnuclide !
-  character(len=18) :: reaction   ! reaction
   character(len=132) :: topline    ! topline
   character(len=15) :: col(10)     ! header
   character(len=15) :: un(10)     ! header
   character(len=80) :: quantity   ! quantity
-  character(len=7)   :: decayfile                     ! decay data file
   character(len=15)  :: gammafile                     ! file with gamma decay data
-  character(len=10)  :: discretefile                  ! file with discrete level data
   integer            :: A                             ! mass number of target nucleus
   integer            :: i                             ! counter
   integer            :: j                             ! counter
   integer            :: k                             ! designator for particle
   integer            :: l                             ! counter
-  integer            :: N                             ! neutron number of residual nucleus
   integer            :: Nentries                      ! number of entries
   integer            :: Ncol                          ! number of columns
   integer            :: ng                            ! number of gamma lines
   integer            :: Ngam(numlev)                  ! counter for discrete gamma rays
   integer            :: Nix                           ! neutron number index for residual nucleus
-  integer            :: type                          ! particle type
   integer            :: Z                             ! charge number of target nucleus
   integer            :: Zix                           ! charge number index for residual nucleus
   integer            :: plev(numlev, numlev*numlev/2) ! level
@@ -156,45 +150,17 @@ subroutine gammadecay(Zix, Nix)
 !
 ! Write decay information to file
 !
-  un = ''
-  col(1)='E'
-  un(1)='MeV'
-  col(2)='fraction'
-  Ncol=2
   quantity='Cumulative discrete gamma decay'
   Z = ZZ(Zix, Nix, 0)
   A = AA(Zix, Nix, 0)
-  type = 2 * Zix + Nix
-  reaction='('//parsym(k0)//','//parsym(type)//')'
-  decayfile = 'decay.'//parsym(type)
-  massstring='   '
-  write(massstring,'(i3)') A
-  finalnuclide=trim(nuc(Z))//adjustl(massstring)
-  open (unit = 1, file = decayfile, status = 'replace')
-  topline=trim(targetnuclide)//trim(reaction)//trim(finalnuclide)//' '//trim(quantity)
-  call write_header(topline,source,user,date,oformat)
-  call write_target
-  call write_reaction(reaction,0.D0,0.D0,0,0)
-  call write_residual(Z,A,finalnuclide)
-  call write_datablock(quantity,Ncol,nlev(Zix, Nix),col,un)
-  do i = 1, nlev(Zix, Nix)
-    write(1, '(2(i6, 9x), es15.6)') i, Ngam(i), yieldg(i)
-    do j = 1, Ngam(i)
-      write(1, '(2es15.6)') egam(i, j), br(i, j)
-    enddo
-  enddo
-  close (unit = 1)
-  reaction='('//parsym(k0)//',x)'
   resstring='000000'
   write(resstring(1:3),'(i3.3)') Z
   write(resstring(4:6),'(i3.3)') A
   gammafile = 'gamma'//resstring//'.tot'
   open (unit = 1, file = gammafile, status = 'replace')
-  topline=trim(targetnuclide)//trim(reaction)//trim(finalnuclide)//' '//trim(quantity)
+  topline=trim(targetnuclide)//' '//trim(quantity)
   call write_header(topline,source,user,date,oformat)
   call write_target
-  call write_reaction(reaction,0.D0,0.D0,0,0)
-  call write_residual(Z,A,finalnuclide)
   Nentries=0
   do i = 1, nlev(Zix, Nix)
     Nentries = Nentries + 1 + Ngam(i)
@@ -210,55 +176,16 @@ subroutine gammadecay(Zix, Nix)
   col(7)='Gamma_energy'
   un(7)='MeV'
   col(8)='Branch._ratio'
-  Ncol=8
   col(9)='Parent_energy'
   un(9)='MeV'
   col(10)='Daughter_energy'
   un(10)='MeV'
+  Ncol=10
   call write_datablock(quantity,Ncol,Nentries,col,un)
   do i = 1, nlev(Zix, Nix)
     write(1, '(2(i6, 9x, es15.6))') i, edis(Zix, Nix, i), Ngam(i), yieldg(i)
     do j = 1, Ngam(i)
       write(1, '(60x, 2(i6, 9x), 4es15.6)') plev(i, j), dlev(i, j), egam(i, j), br(i, j), penergy(i, j), denergy(i, j)
-    enddo
-  enddo
-  close (unit = 1)
-!
-! Write discrete level file for gamma transition probabilities
-!
-  N = A - Z
-  discretefile = 'discrete.'//parsym(type)
-  open (unit = 1, file = discretefile, status = 'replace')
-  quantity='Discrete levels'
-  topline=trim(targetnuclide)//trim(reaction)//' '//trim(quantity)
-  un = ''
-  col(1)='Level'
-  col(2)='Energy'
-  un(2)='MeV'
-  col(3)='Spin'
-  col(4)='Parity'
-  col(5)='Branch._ratio'
-  un(5)='%'
-  col(6)='Half-life'
-  un(6)='sec'
-  col(7)='Assignment'
-  col(8)='ENSDF'
-  Ncol=8
-  call write_header(topline,source,user,date,oformat)
-  call write_target
-  call write_datablock(quantity,Ncol,nlev(Zix, Nix),col,un)
-  do i = 0, nlev(Zix, Nix)
-    if (tau(Zix, Nix, i) /= 0.) then
-      write(1, '(1x, i3, 1x, es12.5, 1x, f4.1, 3x, a1, 9x, i2, 15x,  es10.3, 7x, 2a1, a18)') levnum(Zix, Nix, i),  &
- &      edis(Zix, Nix, i), jdis(Zix, Nix, i), cparity(parlev(Zix, Nix, i)), nbranch(Zix, Nix, i), tau(Zix, Nix, i), &
- &      jassign(Zix, Nix, i), passign(Zix, Nix, i), ENSDF(Zix, Nix, i)
-    else
-      write(1, '(1x, i3, 1x, es12.5, 1x, f4.1, 3x, a1, 9x, i2, 32x, 2a1, a18)') i, edis(Zix, Nix, i), jdis(Zix, Nix, i), &
- &      cparity(parlev(Zix, Nix, i)), nbranch(Zix, Nix, i), jassign(Zix, Nix, i), passign(Zix, Nix, i), ENSDF(Zix, Nix, i)
-    endif
-    do k = 1, nbranch(Zix, Nix, i)
-      write(1, '(33x, "--->", i3, 2x, f8.4, 18x, a1)') branchlevel(Zix, Nix, i, k), branchratio(Zix, Nix, i, k) * 100., &
- &      bassign(Zix, Nix, i, k)
     enddo
   enddo
   close (unit = 1)
