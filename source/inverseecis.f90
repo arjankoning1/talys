27f064f4cc16aa33a3bdbee18446dfb4810bb4ad
Arjan Koning <arjankoning1@gmail.com>
2024-06-09 16:53:57 +0200

    More YANDF output

--- source/inverseecis.f90
+++ source/inverseecis.f90
@@ -152,6 +152,13 @@ subroutine inverseecis(Zcomp, Ncomp)
   logical            :: jlmloc         ! flag for JLM OMP
   logical            :: rotational     ! flag for rotational input
   logical            :: vibrational    ! flag for vibrational input
+  character(len=3)   :: massstring !
+  character(len=6)   :: finalnuclide !
+  character(len=8)   :: ompfile !
+  character(len=15)  :: col(20)    ! header
+  character(len=15)  :: un(20)    ! header
+  character(len=80)  :: quantity   ! quantity
+  character(len=132) :: topline   ! topline
   character(len=132) :: outfile        ! output file
   integer            :: A              ! mass number of target nucleus
   integer            :: i              ! counter
@@ -160,6 +167,7 @@ subroutine inverseecis(Zcomp, Ncomp)
   integer            :: Ncomp          ! neutron number index for compound nucleus
   integer            :: nen            ! energy counter
   integer            :: Nix            ! neutron number index for residual nucleus
+  integer            :: Ncol
   integer            :: type           ! particle type
   integer            :: Z              ! charge number of target nucleus
   integer            :: Zcomp          ! proton number index for compound nucleus
@@ -211,6 +219,47 @@ subroutine inverseecis(Zcomp, Ncomp)
 ! Output of optical model parameters, if requested.
 !
     if (flagoutomp .and. .not. jlmloc) then
+      un='fm'
+      col(1)='E'
+      col(2)='V'
+      un(2)='MeV'
+      col(3)='rv'
+      col(4)='av'
+      col(5)='W'
+      un(5)='MeV'
+      col(6)='rw'
+      col(7)='aw'
+      col(8)='Vd'
+      un(8)='MeV'
+      col(9)='rvd'
+      col(10)='avd'
+      col(11)='Wd'
+      un(11)='MeV'
+      col(12)='rwd'
+      col(13)='awd'
+      col(14)='Vso'
+      un(14)='MeV'
+      col(15)='rvso'
+      col(16)='avso'
+      col(17)='Wso'
+      un(17)='MeV'
+      col(18)='rwso'
+      col(19)='awso'
+      col(20)='rc'
+      Ncol=20
+      quantity='optical model parameters'
+      massstring='   '
+      write(massstring,'(i3)') A
+      finalnuclide=trim(nuc(Z))//adjustl(massstring)
+      topline=trim(finalnuclide)//' '//parname(type)//' '//trim(quantity)
+      ompfile='omppar.'//parsym(type)
+      open (unit=1, file=ompfile, status='replace')
+      call write_header(topline,source,user,date,oformat)
+      call write_residual(Z,A,finalnuclide)
+      write(1,'("# parameters:")') 
+      call write_char(2,'particle',parname(type))
+      Nen =  eendmax(type) - ebegin(type) + 1
+      call write_datablock(quantity,Ncol,Nen,col,un)
       write(*, '(/11x, a8, " on ", i3, a2/)') parname(type), A, nuc(Z)
       write(*, '("  Energy", 5x, "V", 5x, "rv", 4x, "av", 4x, "W", 5x, "rw", 4x, "aw", 4x, "Vd", 3x, "rvd", 3x, "avd", 4x, &
  &      "Wd", 3x, "rwd", 3x, "awd", 3x, "Vso", 3x, "rvso", 2x, "avso", 2x, "Wso", 3x, "rwso", 2x, "awso", 2x, "rc", /)')
@@ -346,6 +395,8 @@ subroutine inverseecis(Zcomp, Ncomp)
 !
       call optical(Zix, Nix, type, e)
       if (flagoutomp .and. .not. jlmloc) then
+        write(1, '(20es15.6)') &
+ &        e, v, rv, av, w, rw, aw, vd, rvd, avd, wd, rwd, awd, vso, rvso, avso, wso, rwso, awso, rc
         write(*, '(1x, f8.3, 1x, 6(f6.2, f6.3, f6.3), f6.3)') &
  &        e, v, rv, av, w, rw, aw, vd, rvd, avd, wd, rwd, awd, vso, rvso, avso, wso, rwso, awso, rc
       endif
@@ -374,6 +425,7 @@ subroutine inverseecis(Zcomp, Ncomp)
       flagecisinp = .true.
       call ecisinput(Zix, Nix, type, e, rotational, vibrational, jlmloc)
     enddo
+    if (flagoutomp .and. .not. jlmloc) close(unit =1)
   enddo
   flaginvecis = .false.
   if ( .not. flageciscalc) return
--- source/inverseout.f90
+++ source/inverseout.f90
@@ -42,28 +42,77 @@ subroutine inverseout(Zcomp, Ncomp)
 ! *** Declaration of local data
 !
   implicit none
+  character(len=3)   :: massstring !
+  character(len=6)   :: finalnuclide !
+  character(len=8)   :: tjlfile !
+  character(len=7)   :: crossfile !
+  character(len=15)  :: col(5)    ! header
+  character(len=15)  :: un(5)    ! header
+  character(len=80)  :: quantity   ! quantity
+  character(len=132) :: topline   ! topline
   integer   :: l                ! multipolarity
   integer   :: Ncomp            ! neutron number index for compound nucleus
   integer   :: nen              ! energy counter
   integer   :: Nix              ! neutron number index for residual nucleus
+  integer   :: Ncol
   integer   :: type             ! particle type
   integer   :: Zcomp            ! proton number index for compound nucleus
   integer   :: Zix              ! charge number index for residual nucleus
+  integer   :: Z
+  integer   :: A
   real(sgl) :: e                ! energy
 !
 ! **************** Transmission coefficients per energy ****************
 !
-  write(*, '(/" ########## TRANSMISSION COEFFICIENTS AND", " INVERSE REACTION CROSS SECTIONS ##########")')
+  write(*, '(/" ########## TRANSMISSION COEFFICIENTS AND INVERSE REACTION CROSS SECTIONS ##########")')
 !
 ! For each energy, the whole set of transmission coefficients is given as a function of the l-value and spin value.
 !
-  if (flagtransen) then
-    do type = 1, 6
-      if (parskip(type)) cycle
-      Zix = Zindex(Zcomp, Ncomp, type)
-      Nix = Nindex(Zcomp, Ncomp, type)
+  do type = 1, 6
+    if (parskip(type)) cycle
+    if (ebegin(type) >= eend(type)) cycle
+    Zix = Zindex(Zcomp, Ncomp, type)
+    Nix = Nindex(Zcomp, Ncomp, type)
+    Z = ZZ(Zcomp, Ncomp, type)
+    A = AA(Zcomp, Ncomp, type)
+    massstring='   '
+    write(massstring,'(i3)') A
+    finalnuclide=trim(nuc(Z))//adjustl(massstring)
+    un=''
+    col(1)='L'
+    if (type /= 3 .and. type /= 6) then
+      col(2)='T(L-1/2,L)'
+      col(3)='T(L+1/2,L)'
+      col(4)='T(L))'
+      Ncol=4
+    endif
+    if (type == 3) then
+      col(2)='T(L-1,L)'
+      col(3)='T(L,L)'
+      col(4)='T(L+1,L)'
+      col(5)='T(L))'
+      Ncol=5
+    endif
+    if (type == 6) then
+      col(2)='T(L))'
+      Ncol=2
+    endif
+    quantity=parname(type)//' transmission coefficients'
+    topline=trim(finalnuclide)//' '//trim(quantity)
+    tjlfile='transm.'//parsym(type)
+    open (unit=1, file=tjlfile, status='replace')
+    call write_header(topline,source,user,date,oformat)
+    call write_residual(Z,A,finalnuclide)
+    write(1,'("# parameters:")')
+    call write_char(2,'particle',parname(type))
+    if (flagtransen) then
+      Nen =  eend(type) - ebegin(type) + 1
+      call write_integer(2,'number of energies',Nen)
       do nen = ebegin(type), eend(type)
         e = real(egrid(nen) / specmass(Zix, Nix, type))
+        write(1,'("# parameters:")')
+        call write_real(2,'energy [MeV]',e)
+        call write_datablock(quantity,Ncol,lmax(type, nen),col,un)
         write(*, '(/" Transmission coefficients for incident ", a8, " at ", f10.5, " MeV"/)') parname(type), e
 !
 ! 1. Spin 1/2 particles: Neutrons, protons, tritons and Helium-3
@@ -71,6 +120,7 @@ subroutine inverseout(Zcomp, Ncomp)
         if (type /= 3 .and. type /= 6 .and. lmax(type, nen) >= 0) then
           write(*, '("   L   T(L-1/2,L)   T(L+1/2,L)    Tav(L)"/)')
           do l = 0, lmax(type, nen)
+            write(1, '(i6, 9x, 3es15.6)') l, Tjl(type, nen, -1, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
             write(*, '(1x, i3, 3es13.5)') l, Tjl(type, nen, -1, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
           enddo
         endif
@@ -80,6 +130,7 @@ subroutine inverseout(Zcomp, Ncomp)
         if (type == 3 .and. lmax(type, nen) >= 0) then
           write(*, '("   L    T(L-1,L)     T(L,L)       ", "T(L+1,L)     Tav(L)"/)')
           do l = 0, lmax(type, nen)
+            write(1, '(i6, 9x, 4es15.6)') l, Tjl(type, nen, -1, l), Tjl(type, nen, 0, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
             write(*, '(1x, i3, 4es13.5)') l, Tjl(type, nen, -1, l), Tjl(type, nen, 0, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
           enddo
         endif
@@ -89,25 +140,26 @@ subroutine inverseout(Zcomp, Ncomp)
         if (type == 6 .and. lmax(type, nen) >= 0) then
           write(*, '("   L     T(L)"/)')
           do l = 0, lmax(type, nen)
+            write(1, '(i9, 6x, es15.6)') l, Tjl(type, nen, 0, l)
             write(*, '(1x, i3, es13.5)') l, Tjl(type, nen, 0, l)
           enddo
         endif
       enddo
-    enddo
+    else
 !
 ! ************ Transmission coefficients per angular momentum **********
 !
-  else
 !
 ! For each l-value, the whole set of transmission coefficients is given as a function of the energy and spin value.
 !
-    do type = 1, 6
-      if (parskip(type)) cycle
-      Zix = Zindex(Zcomp, Ncomp, type)
-      Nix = Nindex(Zcomp, Ncomp, type)
-      if (ebegin(type) >= eend(type)) cycle
+      Nen =  lmax(type, eend(type)) + 1
+      call write_integer(2,'number of angular L-values',Nen)
       do l = 0, lmax(type, eend(type))
         write(*, '(/" Transmission coefficients for incident ", a8, " and l= ", i2/)') parname(type), l
+        write(1,'("# parameters:")')
+        call write_integer(2,'L-value',l)
+        Nen =  eend(type) - ebegin(type) + 1
+        call write_datablock(quantity,Ncol,Nen,col,un)
 !
 ! 1. Spin 1/2 particles: Neutrons, protons, tritons and Helium-3
 !
@@ -115,6 +167,7 @@ subroutine inverseout(Zcomp, Ncomp)
           write(*, '("    Energy    T(L-1/2,L)   T(L+1/2,L)     ", "Tav(L)"/)')
           do nen = ebegin(type), eend(type)
             e = real(egrid(nen) / specmass(Zix, Nix, type))
+            write(1, '(4es15.6)') e, Tjl(type, nen, - 1, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
             write(*, '(1x, f10.5, 3es13.5)') e, Tjl(type, nen, - 1, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
           enddo
         endif
@@ -125,6 +178,7 @@ subroutine inverseout(Zcomp, Ncomp)
           write(*, '("    Energy     T(L-1,L)     T(L,L)       ", "T(L+1,L)     Tav(L)"/)')
           do nen = ebegin(type), eend(type)
             e = real(egrid(nen) / specmass(Zix, Nix, type))
+            write(1, '(5es15.6)') e, Tjl(type, nen, - 1, l), Tjl(type, nen, 0, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
             write(*, '(1x, f10.5, 4es13.5)') e, Tjl(type, nen, - 1, l), Tjl(type, nen, 0, l), Tjl(type, nen, 1, l), Tl(type, nen, l)
           enddo
         endif
@@ -135,25 +189,46 @@ subroutine inverseout(Zcomp, Ncomp)
           write(*, '("    Energy      T(L)"/)')
           do nen = ebegin(type), eend(type)
             e = real(egrid(nen) / specmass(Zix, Nix, type))
+            write(1, '(2es15.6)') e, Tjl(type, nen, 0, l)
             write(*, '(1x, f10.5, es13.5)') e, Tjl(type, nen, 0, l)
           enddo
         endif
       enddo
-    enddo
-  endif
+    endif
+    close(1)
 !
 ! **************** Cross sections for inverse channels *****************
 !
-  do type = 1, 6
-    if (parskip(type)) cycle
-    if (ebegin(type) >= eend(type)) cycle
-    Zix = Zindex(Zcomp, Ncomp, type)
-    Nix = Nindex(Zcomp, Ncomp, type)
+    quantity=parname(type)//' inverse reaction cross sections'
+    topline=trim(finalnuclide)//' '//trim(quantity)
+    crossfile='cross.'//parsym(type)
+    un='mb'
+    col(1)='E'
+    un(1)='MeV'
+    if (type == 1) then
+      col(2)='total'
+      col(3)='elastic'
+      col(4)='reaction'
+      col(5)='OMP reaction'
+      Ncol=5
+    else
+      col(2)='reaction'
+      col(3)='OMP reaction'
+      Ncol=3
+    endif
+    open (unit=1, file=crossfile, status='replace')
+    call write_header(topline,source,user,date,oformat)
+    call write_residual(Z,A,finalnuclide)
+    write(1,'("# parameters:")')
+    call write_char(2,'particle',parname(type))
+    Nen =  eend(type) - ebegin(type) + 1
+    call write_datablock(quantity,Ncol,Nen,col,un)
     if (type == 1) then
       write(*, '(/" Total cross sections for ", a8/)') parname(1)
       write(*, '("      E        total      reaction    elastic", "   OMP reaction"/)')
-      do nen = ebegin(1), eend(type)
+      do nen = ebegin(type), eend(type)
         e = real(egrid(nen) / specmass(Zix, Nix, type))
+        write(1, '(5es15.6)') e, xstot(1, nen), xselas(1, nen), xsreac(1, nen), xsopt(1, nen)
         write(*, '(1x, f10.5, 4es12.4)') e, xstot(1, nen), xsreac(1, nen), xselas(1, nen), xsopt(1, nen)
       enddo
     else
@@ -161,9 +236,11 @@ subroutine inverseout(Zcomp, Ncomp)
       write(*, '("      E       reaction  OMP reaction"/)')
       do nen = ebegin(type), eend(type)
         e = real(egrid(nen) / specmass(Zix, Nix, type))
+        write(1, '(3es15.6)') e, xsreac(type, nen), xsopt(type, nen)
         write(*, '(1x, f10.5, 2es12.4)') e, xsreac(type, nen), xsopt(type, nen)
       enddo
     endif
+    close(1)
   enddo
   return
 end subroutine inverseout
