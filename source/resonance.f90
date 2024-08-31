5db37983dfacaefc6ce32597644e07a5d25c4665
Arjan Koning <arjankoning1@gmail.com>
2024-08-31 10:16:18 +0200

    secured array bound for isochar

--- source/resonance.f90
+++ source/resonance.f90
@@ -111,7 +111,7 @@
     afile = '000.res. '
   else
     afile = '000i.res.'
-    write(afile(4:4), '(a1)') isochar(Liso)
+    write(afile(4:4), '(a1)') isochar(min(Liso,numisom))
   endif
   write(afile(1:3), '(i3.3)') Atarget
   resfile = 'n-'//trim(nuc(Ztarget))//trim(afile)//trim(reslib)
