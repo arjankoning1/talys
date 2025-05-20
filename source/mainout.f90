f5e758f4b1465c4626c6e42dcad559d38430ed35
Arjan Koning <arjankoning1@gmail.com>
2025-05-20 14:33:34 +0200

    version

--- source/mainout.f90
+++ source/mainout.f90
@@ -6,7 +6,7 @@ subroutine mainout
 ! Author    : Arjan Koning
 !
 ! 2023-12-30: Original code
-! 2025-05-05: Current revision
+! 2025-05-20: Current revision
 !-----------------------------------------------------------------------------------------------------------------------------------
 !
 ! *** Use data from other modules
@@ -69,7 +69,7 @@ subroutine mainout
 !
 ! *************************** Code and version *************************
 !
-  write(*, '(/"    TALYS-2.1 (Version: May 5, 2025)"/)')
+  write(*, '(/"    TALYS-2.1 (Version: May 20, 2025)"/)')
   write(*, '(" Copyright (C) 2025  A.J. Koning, S. Hilaire and S. Goriely"/)')
   write(*, '(" Dimensions - Cross sections: mb, Energies: MeV, Angles: degrees")')
   write(*, '(/" User: ",a)') trim(user)
