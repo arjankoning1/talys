6ef0fbc65d8af4985dbd18f41a971c335129ba3d
Arjan Koning <arjankoning1@gmail.com>
2025-08-14 17:45:36 +0200

    version

--- source/talys.f90
+++ source/talys.f90
@@ -6,11 +6,11 @@ program talys
 ! Author    : Arjan Koning, Stephane Hilaire and Stephane Goriely
 !
 ! 2023-12-30: Original code
-! 2025-08-07: Current revision
+! 2025-08-12: Current revision
 !-----------------------------------------------------------------------------------------------------------------------------------
 !
 !   |-------------------------------------------------------|
-!   |                 TALYS-2.12                            |
+!   |                 TALYS-2.13                            |
 !   |                 Arjan Koning                          |
 !   |                 Stephane Hilaire                      |
 !   |                 Stephane Goriely                      |
