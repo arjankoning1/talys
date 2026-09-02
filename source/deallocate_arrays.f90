ed9941dd00fe25e99f698f9e1fa3e191f5aeff3d
Arjan Koning <arjankoning1@gmail.com>
2026-09-02 15:12:02 +0200

    update

--- source/deallocate_arrays.f90
+++ source/deallocate_arrays.f90
@@ -6,14 +6,23 @@ subroutine deallocate_arrays
 !
   use A0_talys_mod
   implicit none
+  integer :: Nix
+  integer :: Zix
 !
   if (allocated(Erescue)) deallocate(Erescue)
   if (allocated(frescue)) deallocate(frescue)
   if (allocated(ddxrec)) deallocate(ddxrec)
   if (allocated(phtable1)) deallocate(phtable1)
   if (allocated(phtable2)) deallocate(phtable2)
-  if (allocated(fqrpa)) deallocate(fqrpa)
-  if (allocated(fqrpaJP)) deallocate(fqrpaJP)
+! if (allocated(fqrpa)) deallocate(fqrpa)
+! if (allocated(fqrpaJP)) deallocate(fqrpaJP)
+  do Nix = 0, numN
+    do Zix = 0, numZ
+      if (allocated(qrpa(Zix,Nix)%e)) deallocate(qrpa(Zix,Nix)%e)
+      if (allocated(qrpa(Zix,Nix)%f)) deallocate(qrpa(Zix,Nix)%f)
+      if (allocated(qrpa(Zix,Nix)%fJP)) deallocate(qrpa(Zix,Nix)%fJP)
+    enddo
+  enddo
 !
   return
 end subroutine deallocate_arrays
